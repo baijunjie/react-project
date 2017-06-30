e3dd8cbc84aa6bc24d1c12390aa77bd6c63329ac
白俊杰 <625603381@qq.com>
2017-07-01 02:25:27 +0800

    热更新

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -17,24 +17,32 @@ export default class extends React.Component {
 
         // 定义组件内使用的 state 数据
         this.state = {
-
+            value: 1
         };
     }
 
     // 数据渲染完成后
     componentDidMount() {
-
+        this.timeout = setTimeout(this.handleTimeoutEvent.bind(this), 1000);
     }
 
     // 组件被卸载前
     componentWillUnmount() {
+        this.timeout && clearTimeout(this.timeout);
+    }
 
+    handleTimeoutEvent() {
+        this.setState((prevState, props) => ({
+            value: prevState.value + 1
+        }), () => {
+            this.timeout = setTimeout(this.handleTimeoutEvent.bind(this), 1000);
+        });
     }
 
     render() {
         return (
             <div style={styles.main}>
-                <h2>{this.title}</h2>
+                <h2>{this.title} : {this.state.value}</h2>
             </div>
         )
     }
